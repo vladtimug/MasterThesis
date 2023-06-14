e60fee3c75c0f6ca3f0a5b71e9322aa7a7794d34
Vlad Timu <timuvlad@yahoo.com>
2023-06-14 12:12:10 +0300

    add script to plot and visualize inference results on test set

---
+++ LiTS/src/plot_inference_results.py
@@ -0,0 +1,112 @@
+import numpy as np
+import pandas as pd
+import os, argparse, tqdm
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from glob import glob
+from preprocessing_utils import normalize, reorient_to_match_training
+
+def parse_script_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--test_data_dir_root_path",
+        type=str, help="Path to the root directory of the test dataset",
+        default="/home/tvlad/Downloads/test_dataset/Test_Data_3Dircadb1/"
+    )
+    
+    parser.add_argument(
+        "--inference_dir_root_path",
+        type=str, help="Path to the root directory of the test dataset inference results",
+        default="/home/tvlad/Projects/MasterThesis/LiTS/experiments_data/set_4/lesion/experiment_9/test_results/"
+    )
+
+    parser.add_argument(
+        "--output_dir_path",
+        type=str, help="Path to the root directory where results will be stored",
+        default="/home/tvlad/Projects/MasterThesis/LiTS/experiments_data/set_4/lesion/experiment_9/test_results_viz/"
+    )
+
+    return parser.parse_args()
+
+def plot_mask_vs_prediction(scan_slice, mask, prediction, save_path, figure_title):
+    multi_channel_slice = np.stack(3 * [scan_slice], axis=2)
+
+    # true positive predictions = green
+    intersection = mask * prediction
+    multi_channel_slice[np.nonzero(intersection)] = (0.0, 1.0, 0.0)
+    
+    # false positive predictions = red
+    multi_channel_slice[np.nonzero((mask == 0) * (prediction == 1))] = (1.0, 0.0, 0.0)
+
+    # false negative prediction = blue
+    multi_channel_slice[np.nonzero((mask == 1) * (prediction == 0))] = (0.0, 0.0, 1.0)
+
+    plt.tight_layout()
+    fig, axs = plt.subplots(2, 2, figsize=(10, 10))
+
+    axs[0, 0].imshow(scan_slice, cmap="gray")
+    axs[0, 0].set_title("Scan Slice")
+
+    axs[0, 1].imshow(mask, cmap="gray")
+    axs[0, 1].set_title("Slice Mask")
+
+    axs[1, 0].imshow(prediction, cmap="gray")
+    axs[1, 0].set_title("Slice Prediction")
+
+    axs[1, 1].imshow(multi_channel_slice)
+    axs[1,1].set_title("Mask vs Prediction")
+
+    plt.suptitle(figure_title)
+    plt.legend(handles=[
+        mpatches.Patch(color="lawngreen", label="True Positives"),
+        mpatches.Patch(color="red", label="False Positives"),
+        mpatches.Patch(color="blue", label="False Negatives")
+        ])
+    plt.savefig(save_path)
+    plt.close()
+
+if __name__ == "__main__":
+    script_args = parse_script_arguments()
+    
+    if not os.path.exists(script_args.output_dir_path):
+        os.mkdir(script_args.output_dir_path)
+
+    MASKS_FILENAME = "2D_LesionMasks.csv"
+    VOLUMES_FILENAME = "2D_Volumes.csv"
+    MASKS_DIRNAME = "LesionMasks"
+    VOLUMES_DIRNAME = "Volumes"
+
+    volume_masks = pd.read_csv(os.path.join(script_args.test_data_dir_root_path, MASKS_FILENAME), header=0)
+    volume_slices = pd.read_csv(os.path.join(script_args.test_data_dir_root_path, VOLUMES_FILENAME), header=0)
+    
+    available_volumes = pd.unique(volume_masks["Volume"])
+    for volume_name in tqdm.tqdm(available_volumes, total=len(available_volumes)):
+        # Create output directory
+        output_volume_dir_path = os.path.join(script_args.output_dir_path, volume_name)
+        if not os.path.exists(output_volume_dir_path):
+            os.mkdir(output_volume_dir_path)
+
+        # Set path to inference results directory
+        inference_dirpath = os.path.join(script_args.inference_dir_root_path, volume_name)
+        filtered_predictions = sorted(glob(inference_dirpath + "/*.npy"), key=lambda elem: int(elem.split("/")[-1].split("-")[-1].split(".")[0]))
+
+        # Extract all relevant paths
+        filtered_masks = volume_masks[volume_masks["Volume"] == volume_name]
+        filtered_slices = volume_slices[volume_slices["Volume"] == volume_name]
+
+        # Sanity check for equivalent number of entries
+        if not(len(os.listdir(inference_dirpath)) == len(filtered_masks) == len(filtered_slices)):
+            raise Exception("Found invalid number of entries among the inference directory content, the number of volume masks and the number of volume slices. They must be equal.")
+
+        # create inference visualization for each slice in volume
+        for slice_path, mask_path, prediction_path in zip(filtered_slices["Slice Path"], filtered_masks["Slice Path"], filtered_predictions):
+            scan_slice = normalize(np.load(slice_path), zero_center=False, unit_variance=False)
+            slice_mask = np.load(mask_path)
+            res = np.load(prediction_path)
+            slice_prediction = reorient_to_match_training(res)
+
+            slice_name = slice_path.split("/")[-1].split(".")[0]
+            plot_path = os.path.join(output_volume_dir_path, slice_name)
+            figure_title = volume_name + " " + slice_name
+            plot_mask_vs_prediction(scan_slice, slice_mask, slice_prediction, plot_path, figure_title)
+            
---
+++ LiTS/src/visualize_inference_results.py
@@ -0,0 +1,63 @@
+import os
+import cv2 as cv
+import numpy as np
+import streamlit as st
+
+st.set_page_config(layout="wide")
+st.title("Liver Tumor Segmentation Results Comparison")
+
+available_volumes = [
+    "volume-1", "volume-2", "volume-3", "volume-3", "volume-4", "volume-6", "volume-8", "volume-9",
+    "volume-10", "volume-12", "volume-13", "volume-15", "volume-16", "volume-17", "volume-19"
+    ]
+
+available_models = {
+    "UNet": {
+        "results_root_path":"/home/tvlad/Projects/MasterThesis/LiTS/experiments_data/set_3/lesion/",
+        "carthesian_model_results_dirname": "experiment_7",
+        "polar_model_results_dirname": "experiment_6"
+        },
+    "UNet++": {
+        "results_root_path": "/home/tvlad/Projects/MasterThesis/LiTS/experiments_data/set_4/lesion/",
+        "carthesian_model_results_dirname": "experiment_8",
+        "polar_model_results_dirname": "experiment_9"
+        },
+    "DeepLabV3+": {
+        "results_root_path": "/home/tvlad/Projects/MasterThesis/LiTS/experiments_data/set_6/lesion/",
+        "carthesian_model_results_dirname": "experiment_12",
+        "polar_model_results_dirname": "experiment_13"
+        }
+    }
+
+TEST_RESULTS_DIRNAME = "test_results_viz"
+
+with st.expander("Filters"):
+    model_arch = st.selectbox("Model Architecture", list(available_models.keys()))
+    test_volume = st.selectbox("Volume", available_volumes)
+    
+model_dict = available_models[model_arch]
+
+carthesian_results_root_dirpath = os.path.join(model_dict["results_root_path"], model_dict["carthesian_model_results_dirname"], TEST_RESULTS_DIRNAME, test_volume)
+polar_results_root_path = os.path.join(model_dict["results_root_path"], model_dict["polar_model_results_dirname"], TEST_RESULTS_DIRNAME, test_volume)
+
+if len(os.listdir(carthesian_results_root_dirpath)) != len(os.listdir(polar_results_root_path)):
+    raise Exception("Inconsistent number of results")
+
+first_slice_idx, last_slice_idx = st.select_slider(
+    "Select Volume Scan Slice Range",
+    options=list(range(len(os.listdir(carthesian_results_root_dirpath)))),
+    value=(0, 5)
+)
+
+for slice_idx in range(first_slice_idx, last_slice_idx):
+    slice_name = f"slice-{slice_idx}.png"
+
+    carthesian_res = cv.imread(os.path.join(carthesian_results_root_dirpath, slice_name))
+    polar_res = cv.imread(os.path.join(polar_results_root_path, slice_name))
+
+    st.image(
+        image=[carthesian_res, polar_res],
+        caption=["Carthesian Model", "Polar Model"],
+        channels="BGR",
+        width=860
+        )
